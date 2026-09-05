2b44bfefda5d1ab845cd55033dd9f933a31a2116
Faizalfadaa <faizalfadha@gmail.com>
2026-09-05 11:11:00 +0700

    feat: implement database

---
+++ apps/backend/src/database/index.ts
@@ -0,0 +1,6 @@
+export { prisma } from "./prisma";
+export * from "./repositories/users.repository";
+export * from "./repositories/workspace.repository";
+export * from "./repositories/chatMessage.repository";
+export * from "./repositories/checkpoint.repository";
+export * from "./repositories/report.repository";
